6f2fdbdc66bdb6f81b8b385401d7502e64b5705a
森雄士郎 <130552004+yu371@users.noreply.github.com>
2024-09-20 17:06:26 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -116,6 +116,7 @@ class _MyHomePageState extends State<MyHomePage> {
     signInAnonymously();
     WidgetsBinding.instance!.addPostFrameCallback(
       (_) => _showStartDialog(),
+      );
   }
 
   Future<void> signInAnonymously() async {
